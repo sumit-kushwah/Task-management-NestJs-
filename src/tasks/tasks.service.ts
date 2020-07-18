6fbdc5b9aa914082ee79ae9f426908cb10f17622
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-07-18 06:09:25 +0530

    delete and updateStatus method created

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -10,7 +10,13 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task {
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    createTask(createTaskDto): Task {
+        const { title, description } = createTaskDto;
+
         const task: Task = {
             id : uuidv1(),
             title,
@@ -19,6 +25,16 @@ export class TasksService {
         }
 
         this.tasks.push(task);
+        return task;
+    }
+
+    deleteTask(id: string): void {
+       this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = status;
         return task;
     }
 }
